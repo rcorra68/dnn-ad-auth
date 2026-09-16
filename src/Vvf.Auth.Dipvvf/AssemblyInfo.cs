fe09163fab4d0e868719abbdcc7e9dad33f372f6
Roberto Corradetti <rcorra68@gmail.com>
2026-09-16 11:31:02 +0200

    feat(project): initialize c# project file and assembly info for active directory auth module
    - Replace VB.NET .vbproj with C# .csproj targeting .NET Framework 4.0
    - Add System.DirectoryServices.AccountManagement framework assembly reference
    - Convert AssemblyInfo.vb to AssemblyInfo.cs with VVF metadata
    - Configure output target to artifacts directory

---
+++ src/Vvf.Auth.Dipvvf/AssemblyInfo.cs
@@ -0,0 +1,30 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DotNetNuke Active Directory Authentication Provider")]
+[assembly: AssemblyDescription("VVF Active Directory Authentication Provider for DotNetNuke")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("VVF")]
+[assembly: AssemblyProduct("Vvf.Auth.Dipvvf")]
+[assembly: AssemblyCopyright("Copyright © VVF 2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components. If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("334e9cee-0d47-4d70-924b-b5098a3432cb")]
+
+// Version information for an assembly consists of the following four values:
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+[assembly: AssemblyVersion("06.00.08.1")]
+[assembly: AssemblyFileVersion("06.00.08.1")]
