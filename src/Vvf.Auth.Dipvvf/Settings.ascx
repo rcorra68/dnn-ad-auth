de42cd63fbe9427f210b95d66277a34acda3cdbb
Roberto Corradetti <rcorra68@gmail.com>
2026-09-16 11:36:08 +0200

    refactor(auth-ad): migrate Settings control from VB.NET to C#
    - Convert Settings.ascx.vb to Settings.ascx.cs following C# standards
    - Re-generate designer class in C# format
    - Update Control directive in Settings.ascx to reference C# codebehind

---
+++ src/Vvf.Auth.Dipvvf/Settings.ascx
@@ -0,0 +1,2 @@
+<%@ Control Language="C#" AutoEventWireup="false" Explicit="True" Inherits="DotNetNuke.Authentication.ActiveDirectory.Settings"
+    Codebehind="Settings.ascx.cs" %>
---
+++ src/Vvf.Auth.Dipvvf/Settings.ascx.cs
@@ -0,0 +1,310 @@
+namespace DotNetNuke.Authentication.ActiveDirectory
+{
+    using System;
+    using System.Collections;
+    using System.Web;
+    using System.Web.UI.WebControls;
+    using DotNetNuke.Authentication.ActiveDirectory.ADSI;
+    using DotNetNuke.Entities.Portals;
+    using DotNetNuke.Framework.Providers;
+    using DotNetNuke.Services.Authentication;
+    using DotNetNuke.Services.Exceptions;
+    using DotNetNuke.Services.Localization;
+    using DotNetNuke.UI.Skins.Controls;
+
+    public partial class Settings : AuthenticationSettingsBase
+    {
+        private string _strError = string.Empty;
+
+        #region Private Methods
+
+        private void DisplayIpError(string strInvalidIP)
+        {
+            string strError = $"{strInvalidIP} {Localization.GetString("InValidIPAddress", LocalResourceFile)}";
+            tblSettings.Visible = true;
+            pnlError.Visible = true;
+            lblError.Text = strError;
+        }
+
+        private string GetUserDomainName(string userName)
+        {
+            int index = userName.IndexOf(@"\", StringComparison.Ordinal);
+            return index > 0 ? userName.Substring(0, index) : string.Empty;
+        }
+
+        private string LocalizedStatus(string inputText)
+        {
+            string strReturn = inputText;
+            strReturn = strReturn.Replace("[Global Catalog Status]", Localization.GetString("[Global Catalog Status]", LocalResourceFile));
+            strReturn = strReturn.Replace("[Root Domain Status]", Localization.GetString("[Root Domain Status]", LocalResourceFile));
+            strReturn = strReturn.Replace("[LDAP Status]", Localization.GetString("[LDAP Status]", LocalResourceFile));
+            strReturn = strReturn.Replace("[Network Domains Status]", Localization.GetString("[Network Domains Status]", LocalResourceFile));
+            strReturn = strReturn.Replace("[LDAP Error Message]", Localization.GetString("[LDAP Error Message]", LocalResourceFile));
+            strReturn = strReturn.Replace("OK", Localization.GetString("OK", LocalResourceFile));
+            strReturn = strReturn.Replace("FAIL", Localization.GetString("FAIL", LocalResourceFile));
+            return strReturn;
+        }
+
+        private bool CheckEnteredIPAddr()
+        {
+            if (txtAutoIP.Text.EndsWith(";"))
+            {
+                txtAutoIP.Text = txtAutoIP.Text.Substring(0, txtAutoIP.Text.Length - 1);
+            }
+
+            var arrIPArray = new ArrayList();
+            string[] arrAutoIP = txtAutoIP.Text.Split(';');
+
+            foreach (string strAutoIP in arrAutoIP)
+            {
+                if (strAutoIP.Contains("-"))
+                {
+                    string[] arrIPRange = strAutoIP.Split('-');
+                    foreach (string ipRangeItem in arrIPRange)
+                    {
+                        int intFullIPAddr = ipRangeItem.Split('.').GetUpperBound(0);
+                        if (intFullIPAddr == 3)
+                        {
+                            arrIPArray.Add(ipRangeItem);
+                        }
+                        else
+                        {
+                            DisplayIpError(ipRangeItem);
+                            return false;
+                        }
+                    }
+                }
+                else
+                {
+                    int intFullIPAddr = strAutoIP.Split('.').GetUpperBound(0);
+                    if (intFullIPAddr == 3)
+                    {
+                        arrIPArray.Add(strAutoIP);
+                    }
+                    else
+                    {
+                        DisplayIpError(strAutoIP);
+                        return false;
+                    }
+                }
+            }
+
+            foreach (string ipToCheck in arrIPArray)
+            {
+                try
+                {
+                    string strIPAddr = Utilities.GetIP4Address(ipToCheck);
+                }
+                catch (Exception)
+                {
+                    DisplayIpError(ipToCheck);
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public override void UpdateSettings()
+        {
+            var portalSettings = (PortalSettings)HttpContext.Current.Items["PortalSettings"];
+            try
+            {
+                if (!chkAuthentication.Checked)
+                {
+                    Configuration.UpdateConfig(portalSettings.PortalId, false, false, "", "", "", "", false, false, false, "", "", "", "", false, "", false);
+                    Configuration.ResetConfig();
+                }
+                else
+                {
+                    string providerTypeName = cboProviders.SelectedItem.Value;
+                    string authenticationType = cboAuthenticationType.SelectedItem.Value;
+
+                    if (!string.IsNullOrEmpty(txtAutoIP.Text))
+                    {
+                        if (!CheckEnteredIPAddr())
+                        {
+                            return;
+                        }
+                    }
+
+                    if (chkAuthentication.Checked && !chkHidden.Checked)
+                    {
+                        Configuration.UpdateConfig(
+                            portalSettings.PortalId,
+                            chkAuthentication.Checked,
+                            chkHidden.Checked,
+                            txtRootDomain.Text,
+                            txtEmailDomain.Text,
+                            txtUserName.Text,
+                            txtPassword.Text,
+                            chkSynchronizeRole.Checked,
+                            chkSynchronizePassword.Checked,
+                            chkStripDomainName.Checked,
+                            providerTypeName,
+                            authenticationType,
+                            txtAutoIP.Text,
+                            txtDefaultDomain.Text,
+                            chkAutoCreate.Checked,
+                            txtBots.Text,
+                            chkSynchronizePhoto.Checked);
+                    }
+                    else
+                    {
+                        Configuration.UpdateConfig(
+                            portalSettings.PortalId,
+                            false,
+                            chkHidden.Checked,
+                            txtRootDomain.Text,
+                            txtEmailDomain.Text,
+                            txtUserName.Text,
+                            txtPassword.Text,
+                            chkSynchronizeRole.Checked,
+                            chkSynchronizePassword.Checked,
+                            chkStripDomainName.Checked,
+                            providerTypeName,
+                            authenticationType,
+                            txtAutoIP.Text,
+                            txtDefaultDomain.Text,
+                            chkAutoCreate.Checked,
+                            txtBots.Text,
+                            chkSynchronizePhoto.Checked);
+                    }
+
+                    Configuration.ResetConfig();
+                    var objAuthenticationController = new AuthenticationController();
+                    string statusMessage = objAuthenticationController.NetworkStatus;
+
+                    if (statusMessage.ToLower().Contains("fail"))
+                    {
+                        MessageCell.Controls.Add(Skins.Skin.GetModuleMessageControl("", LocalizedStatus(statusMessage), ModuleMessage.ModuleMessageType.RedError));
+                    }
+                    else
+                    {
+                        MessageCell.Controls.Add(Skins.Skin.GetModuleMessageControl("", LocalizedStatus(statusMessage), ModuleMessage.ModuleMessageType.GreenSuccess));
+                    }
+                }
+            }
+            catch (Exception exc)
+            {
+                Exceptions.ProcessModuleLoadException(this, exc);
+            }
+        }
+
+        #endregion
+
+        #region Event Handlers
+
+        protected void Page_Init(object sender, EventArgs e)
+        {
+            var objAuthenticationController = new AuthenticationController();
+            ProviderConfiguration objProviderConfiguration = ProviderConfiguration.GetProviderConfiguration(Configuration.AUTHENTICATION_KEY);
+
+            foreach (DictionaryEntry objProvider in objProviderConfiguration.Providers)
+            {
+                string providerName = (string)objProvider.Key;
+                string providerType = ((Provider)objProvider.Value).Type;
+                cboProviders.Items.Add(new ListItem(providerName, providerType));
+            }
+
+            try
+            {
+                cboAuthenticationType.DataSource = objAuthenticationController.AuthenticationTypes;
+            }
+            catch (TypeInitializationException)
+            {
+                _strError = Localization.GetString("AuthProviderError", LocalResourceFile);
+            }
+
+            cboAuthenticationType.DataBind();
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                AspNetHostingPermissionLevel permission = Utilities.GetCurrentTrustLevel();
+                if (permission != AspNetHostingPermissionLevel.Unrestricted)
+                {
+                    Response.Redirect("~/DesktopModules/AuthenticationServices/ActiveDirectory/trusterror.htm", true);
+                }
+                else
+                {
+                    PortalSettings portalSettings = PortalController.Instance.GetCurrentPortalSettings();
+
+                    Configuration.ResetConfig();
+                    Configuration config = Configuration.GetConfig();
+
+                    if (UserInfo.Username.IndexOf(@"\", StringComparison.Ordinal) > 0)
+                    {
+                        string strDomain = GetUserDomainName(UserInfo.Username);
+                        if (string.Equals(strDomain, Request.ServerVariables["SERVER_NAME"], StringComparison.OrdinalIgnoreCase))
+                        {
+                            _strError = string.Format(
+                                Localization.GetString("SameDomainError", LocalResourceFile),
+                                strDomain,
+                                HttpUtility.HtmlEncode(Request.ServerVariables["SERVER_NAME"]));
+                        }
+                    }
+
+                    if (!IsPostBack)
+                    {
+                        chkAuthentication.Checked = config.WindowsAuthentication;
+                        chkHidden.Checked = config.HideWindowsLogin;
+                        if (chkHidden.Checked)
+                        {
+                            chkAuthentication.Checked = true;
+                        }
+
+                        chkSynchronizeRole.Checked = config.SynchronizeRole;
+                        chkSynchronizePhoto.Checked = config.Photo;
+                        chkSynchronizePassword.Checked = config.SynchronizePassword;
+                        chkStripDomainName.Checked = config.StripDomainName;
+                        txtRootDomain.Text = config.RootDomain;
+                        txtUserName.Text = config.UserName;
+                        txtEmailDomain.Text = config.EmailDomain;
+                        txtAutoIP.Text = config.AutoIP;
+                        txtDefaultDomain.Text = config.DefaultDomain;
+                        chkAutoCreate.Checked = config.AutoCreateUsers;
+                        txtBots.Text = config.Bots;
+
+                        if (string.IsNullOrEmpty(txtBots.Text))
+                        {
+                            txtBots.Text = "gsa-crawler;MS Search 5.0 Robot";
+                        }
+
+                        ListItem selectedType = cboAuthenticationType.Items.FindByText(config.AuthenticationType);
+                        if (selectedType != null)
+                        {
+                            selectedType.Selected = true;
+                        }
+                    }
+
+                    valConfirm.ErrorMessage = Localization.GetString("PasswordMatchFailure", LocalResourceFile);
+
+                    if (string.IsNullOrEmpty(_strError))
+                    {
+                        tblSettings.Visible = true;
+                        pnlError.Visible = false;
+                    }
+                    else
+                    {
+                        tblSettings.Visible = false;
+                        pnlError.Visible = true;
+                        lblError.Text = _strError;
+                    }
+                }
+            }
+            catch (Exception exc)
+            {
+                Exceptions.ProcessModuleLoadException(this, exc);
+            }
+        }
+
+        #endregion
+    }
+}
